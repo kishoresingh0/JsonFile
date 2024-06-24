caf586c315f9e1a4a781f62892c13b88dedbd394
kishore <kishoresingh2025@gmail.com>
2024-06-24 16:31:33 +0530

    Add folder

---
+++ 02/text.js
@@ -0,0 +1,21 @@
+ 
+// var scope or Globle  is declared, accessible anywhere  function internal & external both site in js
+if(true){
+ var age = 23;
+ console.log(age);
+}
+console.log(age);
+
+// let Scope or Local  is declared, accessible and specific function internal site in js
+if(true){
+    let age1 = 24;
+    console.log(age1);
+   }
+
+   // Const scope or local  is declared inaccessible function internal insite and internal block access
+   // values re-assign nhi ho sakti
+if(true){
+    const age2 = 20;
+    console.log(age2);
+   }
+  
---
+++ 03/text.js
@@ -0,0 +1,6 @@
+// Data Types
+// Numbers
+let num = 3.14;
+let num2 = 10;
+// console.log(num+num2);
+
